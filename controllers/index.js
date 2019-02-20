aee658ec85db919e0e93f1e2a4307c88bb340c9d
Leyla <Leyla.Duymazlar@sacw.com>
2019-02-19 21:31:20 -0600

    updating mongo db

--- controllers/index.js
+++ controllers/index.js
@@ -1,29 +1,29 @@
-/*'use strict';
+// 'use strict';
 
-var express = require('express');
-var router = express.Router();
-var Product = require('../models/product.js');
-
-
-router.get('/', function (req, res) {
-    Product .find({}) .where('saved').equals(false) .where('deleted').equals(false)
-        .sort('-date')
-        .limit(10) .exec(function (error, product) {
-            if (error) {
-                console.log(error);
-                res.status(500);
-            } else {
-                console.log(product);
-                let hbsObj = {
-                    title: 'Face Products',
-                    product: product
-                };
-                res.render('index', hbsObj);
-            }
-        });
-});
-*/
+// var express = require('express');
+// var router = express.Router();
+// var Product = require('../models/product.js');
 
 
-router.use('/api', require('./api'));
-module.exports = router;
+// router.get('/', function (req, res) {
+//     Product .find({}) .where('saved').equals(false) .where('deleted').equals(false)
+//         .sort('-date')
+//         .limit(10) .exec(function (error, product) {
+//             if (error) {
+//                 console.log(error);
+//                 res.status(500);
+//             } else {
+//                 console.log(product);
+//                 let hbsObj = {
+//                     title: 'Face Products',
+//                     product: product
+//                 };
+//                 res.render('index', hbsObj);
+//             }
+//         });
+// });
+
+
+
+// router.use('/api', require('./api'));
+// module.exports = router;
