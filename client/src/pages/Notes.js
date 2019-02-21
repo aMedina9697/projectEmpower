23ea60aa2d30c4712348837f8ec8777e95df6ffa
Theo Daly <theo.daly@outlook.com>
2019-02-20 20:07:10 -0600

    notes work on the local db!

--- client/src/pages/Notes.js
+++ client/src/pages/Notes.js
@@ -5,12 +5,12 @@ import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
+//import { Input, TextArea, FormBtn } from "../components/Form";
 
 class Notes extends Component {
   state = {
-    products: [],
-    product:"",
+  products: [],
+  product:"",
   product_brand: "",
   product_price: "",
   product_shade: "",
@@ -25,12 +25,7 @@ class Notes extends Component {
   loadProducts = () => {
     API.getProducts()
       .then(res =>
-        this.setState({ products: res.data, product:"",
-        product_brand: "",
-        product_price: "",
-        product_shade: "",
-        product_type: "",
-        product_note: "" })
+        this.setState({ products: res.data, product:"", product_brand: "", product_price: "", product_shade: "", product_type: "", product_note: "" })
       )
       .catch(err => console.log(err));
   };
@@ -41,90 +36,35 @@ class Notes extends Component {
       .catch(err => console.log(err));
   };
 
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
+  // handleInputChange = event => {
+  //   const { name, value } = event.target;
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // };
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    if (this.state.product && this.state.product_brand && this.state.product_price && this.state.product_shade && this.state.product_type && this.state.product_note) {
-      API.saveProduct({
-        product: this.state.product,
-        product_brand: this.state.product_brand,
-        product_price: this.state.product_price,
-        product_shade: this.state.product_shade,
-        product_type: this.state.product_type,
-        product_note: this.state.product_note
-      })
-        .then(res => this.loadProducts())
-        .catch(err => console.log(err));
-    }
-  };
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   if (this.state.product && this.state.product_brand && this.state.product_price && this.state.product_shade && this.state.product_type && this.state.product_note) {
+  //     API.saveProduct({
+  //       product: this.state.product,
+  //       product_brand: this.state.product_brand,
+  //       product_price: this.state.product_price,
+  //       product_shade: this.state.product_shade,
+  //       product_type: this.state.product_type,
+  //       product_note: this.state.product_note
+  //     })
+  //       .then(res => this.loadProducts())
+  //       .catch(err => console.log(err));
+  //   }
+  // };
 
   render() {
     return (
       <Container fluid>
         <Row>
-          <Col size="md-6">
-            <Jumbotron>
-              <h1>add your products, gorgeous:</h1>
-            </Jumbotron>
-            <form>
-              <Input
-                value={this.state.product}
-                onChange={this.handleInputChange}
-                name="product"
-                placeholder="product name (required)"
-              />
-              <Input
-                value={this.state.product_brand}
-                onChange={this.handleInputChange}
-                name="brand"
-                placeholder="product brand (required)"
-              />
-              <Input
-                value={this.state.product_price}
-                onChange={this.handleInputChange}
-                name="price"
-                placeholder="product price (or estimate)"
-              />
-              <Input
-                value={this.state.product_shade}
-                onChange={this.handleInputChange}
-                name="shade"
-                placeholder="product shade"
-                
-              />
-              <Input
-                value={this.state.product_type}
-                onChange={this.handleInputChange}
-                name="type"
-                placeholder="product type"
-                />
-
-              <TextArea
-                value={this.state.product_note}
-                onChange={this.handleInputChange}
-                name="note"
-                placeholder="thoughts?"
-              />
-              <FormBtn
-                disabled={!(this.state.product && this.state.product_brand && this.state.product_price && this.state.product_shade && this.state.product_type && this.state.product_note)}
-                onClick={this.handleFormSubmit}
-              >
-                submit product!
-              </FormBtn>
-              <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-            </form>
-          </Col>
-          <Col size="md-6 sm-12">
+        <Col size="md-1"></Col>
+          <Col size="md-10">
             <Jumbotron>
               <h1> love notes...</h1>
             </Jumbotron>
@@ -147,7 +87,9 @@ class Notes extends Component {
               <h3> nothing saved yet ;) </h3>
             )}
           </Col>
+          <Col size="md-1"></Col>
         </Row>
+        
       </Container>
     );
   }
--- controllers/productsController.js
+++ controllers/productsController.js
@@ -15,7 +15,7 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
+  create: function(req, res) { console.log(req.body);
     db.Product
       .create(req.body)
       .then(dbModel => res.json(dbModel))
